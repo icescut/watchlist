46d91272cd2c0e720b21159491556c7778834c56
icescut <337988934@qq.com>
2021-12-20 22:13:46 +0800

    添加数据库支持

--- app.py
+++ app.py
@@ -1,18 +1,67 @@
 from flask import Flask, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+import click
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + \
+    os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-name = '小冰'
-movies = [
-    {'title': '放牛班的春天', 'year': 2004},
-    {'title': '摩登时代', 'year': 1936},
-    {'title': '城市之光', 'year': 1931},
-    {'title': '疯狂动物城', 'year': 2016},
-    {'title': '大话西游之大圣娶亲', 'year': 1995},
-]
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
 
 
 @app.route("/")
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
+
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('数据库初始化完成')
+
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = '小冰'
+    movies = [
+        {'title': '放牛班的春天', 'year': 2004},
+        {'title': '摩登时代', 'year': 1936},
+        {'title': '城市之光', 'year': 1931},
+        {'title': '疯狂动物城', 'year': 2016},
+        {'title': '大话西游之大圣娶亲', 'year': 1995},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
