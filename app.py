bb22362e5eada52f4f136f0dd176364b1b487b75
icescut <337988934@qq.com>
2021-12-24 07:16:31 +0800

    增加权限管理

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+from click.termui import prompt
 from flask import Flask, url_for, render_template, flash, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import os
 import sys
 import click
+from werkzeug.security import generate_password_hash, check_password_hash
 
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -16,11 +19,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = prefix + \
     os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Movie(db.Model):
@@ -41,9 +54,17 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
         title = request.form.get('title')
         year = request.form.get('year')
 
@@ -62,7 +83,38 @@ def index():
     return render_template('index.html', movies=movies)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if not username or not password:
+            flash('非法输入')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+        if (username == user.username and user.validate_password(password)):
+            login_user(user)
+            flash('登录成功')
+            return redirect(url_for('index'))
+
+        flash('用户名或密码错误')
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('再见')
+    return redirect(url_for('index'))
+
+
 @app.route('/moive/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
 
@@ -84,12 +136,30 @@ def edit(movie_id):
 
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
     db.session.commit()
     flash('删除成功')
     return redirect(url_for('index'))
+
+
+@app.route('/settings', methods=['GET', 'POST'])
+def settings():
+    if request.method == 'POST':
+        name = request.form.get('name')
+
+        if not name or len(name) > 20:
+            flash('非法输入')
+            return redirect(url_for('settings'))
+
+        current_user.name = name
+        db.session.commit()
+        flash('更新成功')
+        return redirect(url_for('index'))
+
+    return render_template('settings.html')
 
 
 @app.cli.command()
@@ -122,3 +192,24 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='登录名')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='登录密码')
+def admin(username, password):
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('更新用户...')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('创建用户...')
+        user = User(username=username, name="Admin")
+        user.set_password(password)
+        db.session.add(user)
+
+    db.session.commit()
+    click.echo('完成！')
